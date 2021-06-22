4dce778c38ae9f6598235995bd99f437aed9c05c
madoyutaka22 <shingesu19@gmail.com>
2021-06-23 01:32:38 +0900

    Add Calculator.java

---
+++ src/main/java/calc/CalculatorTest.java
@@ -0,0 +1,24 @@
+package calc;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+
+	@Test
+	public void multiplyで乗算結果が取得できる() {
+		Calculator calc = new	Calculator();
+		
+	}
+
+}
---
+++ src/main/java/test/CalculatorTest2.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author satouyutaka
+ *
+ */
+class CalculatorTest2 {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * {@link test.Calculator#multiply(int, int)} のためのテスト・メソッド。
+	 */
+	@Test
+	void testMultiply() {
+		fail("まだ実装されていません");
+	}
+
+	/**
+	 * {@link test.Calculator#divide(int, int)} のためのテスト・メソッド。
+	 */
+	@Test
+	void testDivide() {
+		fail("まだ実装されていません");
+	}
+
+}
